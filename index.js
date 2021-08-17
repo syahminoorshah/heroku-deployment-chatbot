39d7a8c4f544383ce5264d7610cb5607080a1563
syahmishah <syahmishah@wipdata.com>
2021-08-17 16:28:40 +0800

    Cards1

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const config = require('./config/keys');
-const mongoose = require('mongoose');
-mongoose.connect(config.mongoURI, { useNewUrlParser: true});
+// const mongoose = require('mongoose');
+// mongoose.connect(config.mongoURI, { useNewUrlParser: true});
 
 app.use(bodyParser.json());
 
