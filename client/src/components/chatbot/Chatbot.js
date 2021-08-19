a8d0619b290bff593ef91e57131df4f86015f791
syahmishah <syahmishah@wipdata.com>
2021-08-19 10:53:06 +0800

    S69

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import axios from "axios/index";
+import { withRouter } from 'react-router-dom';
 
 import Cookies from 'universal-cookie';
 import { v4 as uuid } from 'uuid';
 
 import Message from './Message';
 import Card from './Card';
+import QuickReplies from './QuickReplies';
 
 const cookies = new Cookies();
 
@@ -17,8 +19,14 @@ class Chatbot extends Component {
         super(props);
         // This binding is necessary to make `this` work in the callback
         this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
+        this._handleQuickReplyPayload = this._handleQuickReplyPayload.bind(this);
+        this.hide = this.hide.bind(this);
+        this.show = this.show.bind(this);
+
         this.state = {
-            messages: []
+            messages: [],
+            showBot: true,
+            shopWelcomeSent: false
         };
         if (cookies.get('userID') === undefined) {
             cookies.set('userID', uuid(), { path: '/' });
@@ -61,13 +69,65 @@ class Chatbot extends Component {
         }
     };
 
-    componentDidMount() {
+    resolveAfterXSeconds(x) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(x);
+            }, x * 1000);
+        })
+    }
+
+    async componentDidMount() {
         this.df_event_query('Welcome');
+
+        if (window.location.pathname === '/shop' && !this.state.shopWelcomeSent) {
+            await this.resolveAfterXSeconds(1);
+            this.df_event_query('WELCOME_SHOP');
+            this.setState({ shopWelcomeSent: true, showBot: true });
+        }
+
+        this.props.history.listen(() => {
+            if (this.props.history.location.pathname === '/shop' && !this.state.shopWelcomeSent) {
+                this.df_event_query('WELCOME_SHOP');
+                this.setState({ shopWelcomeSent: true, showBot: true });
+            }
+        });
     }
 
     componentDidUpdate() {
         this.messagesEnd.scrollIntoView({ behavior: "smooth" });
-        this.talkInput.focus();
+        if ( this.talkInput ) {
+            this.talkInput.focus();
+        }
+    }
+
+    show(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        this.setState({showBot: true});
+    }
+
+    hide(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        this.setState({showBot: false});
+    }
+
+    _handleQuickReplyPayload(event, payload, text) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        switch (payload) {
+            case 'recommended_yes':
+                this.df_event_query('SHOW_RECOMMENDATIONS');
+                break;
+            case 'training_masterclass':
+                this.df_event_query('MASTERCLASS');
+                break;
+            default:
+                this.df_text_query(text);
+                break;
+        }
     }
 
     renderCards(cards) {
@@ -94,6 +154,17 @@ class Chatbot extends Component {
                     </div>
                 </div>
             </div>
+        } else if (message.msg &&
+            message.msg.payload &&
+            message.msg.payload.fields &&
+            message.msg.payload.fields.quick_replies
+        ) {
+            return <QuickReplies
+                text={message.msg.payload.fields.text ? message.msg.payload.fields.text : null}
+                key={i}
+                replyClick={this._handleQuickReplyPayload}
+                speaks={message.speaks}
+                payload={message.msg.payload.fields.quick_replies.listValue.values}/>;
         }
     }
 
@@ -116,26 +187,50 @@ class Chatbot extends Component {
     }
 
     render() {
-        return (
-            <div style={{ minHeight: 500, maxHeight: 500, width:400, position: 'absolute', bottom: 0, right: 0, border: '1px solid lightgray'}}>
-                <nav>
-                    <div className="nav-wrapper">
-                        <a href="/" className="brand-logo">ChatBot</a>
-                    </div>
-                </nav>
+        if (this.state.showBot) {
+            return (
+                <div style={{ minHeight: 500, maxHeight: 500, width:400, position: 'absolute', bottom: 0, right: 0, border: '1px solid lightgray'}}>
+                    <nav>
+                        <div className="nav-wrapper">
+                            <a href="/" className="brand-logo">ChatBot</a>
+                            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                                <li><a href="/" onClick={this.hide}>Close</a></li>
+                            </ul>
+                        </div>
+                    </nav>
 
-                <div id="chatbot"  style={{ minHeight: 388, maxHeight: 388, width:'100%', overflow: 'auto'}}>
-                    {this.renderMessages(this.state.messages)}
+                    <div id="chatbot"  style={{ minHeight: 388, maxHeight: 388, width:'100%', overflow: 'auto'}}>
+
+                        {this.renderMessages(this.state.messages)}
+                        <div ref={(el) => { this.messagesEnd = el; }}
+                             style={{ float:"left", clear: "both" }}>
+                        </div>
+                    </div>
+                    <div className=" col s12" >
+                        <input style={{margin: 0, paddingLeft: '1%', paddingRight: '1%', width: '98%'}} ref={(input) => { this.talkInput = input; }} placeholder="type a message:"  onKeyPress={this._handleInputKeyPress} id="user_says" type="text" />
+                    </div>
+
+                </div>
+            );
+        } else {
+            return (
+                <div style={{ minHeight: 40, maxHeight: 500, width:400, position: 'absolute', bottom: 0, right: 0, border: '1px solid lightgray'}}>
+                    <nav>
+                        <div className="nav-wrapper">
+                            <a href="/" className="brand-logo">ChatBot</a>
+                            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                                <li><a href="/" onClick={this.show}>Show</a></li>
+                            </ul>
+                        </div>
+                    </nav>
                     <div ref={(el) => { this.messagesEnd = el; }}
                          style={{ float:"left", clear: "both" }}>
                     </div>
                 </div>
-                <div className=" col s12" >
-                    <input style={{margin: 0, paddingLeft: '1%', paddingRight: '1%', width: '98%'}} ref={(input) => { this.talkInput = input; }} placeholder="type a message:"  onKeyPress={this._handleInputKeyPress} id="user_says" type="text" />
-                </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
-export default Chatbot;
+//export default Chatbot;
+export default withRouter(Chatbot);
