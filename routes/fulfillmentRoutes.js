8fc1f0057574831cb8b65726e1cf5995a4d42f57
syahmishah <syahmishah@wipdata.com>
2021-10-07 14:21:32 +0800

    phase2-j&t

--- routes/fulfillmentRoutes.js
+++ routes/fulfillmentRoutes.js
@@ -8,17 +8,12 @@ module.exports = app => {
             agent.add(`Welcome to my Roro fulfillment!`);
         }
 
-        function Option1Tracking(agent) {
-            agent.add(`testing`);
-        }
-
         function fallback(agent) {
             agent.add(`I didn't understand`);
             agent.add(`I'm sorry, can you try again?`);
         }
         let intentMap = new Map();
         intentMap.set('roro', roro);
-        intentMap.set('testing',Option1Tracking)
 
         intentMap.set('Default Fallback Intent', fallback);
 
